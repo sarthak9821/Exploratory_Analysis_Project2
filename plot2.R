35fc9ff4388cc5bc146323c0ef5f178fee922c64
Sarthak9821 <sarthak.kawatra98@gmail.com>
2020-09-23 18:51:47 +0530

    Project Commit

---
+++ plot2.R
@@ -0,0 +1,28 @@
+#import needed packages
+library("data.table")
+# import data 
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "//"))
+# I use // as I'm working on windows machine
+unzip(zipfile = "dataFiles.zip")
+
+# Load the NEI & SCC data frames.
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+# Starting a graphic device 
+
+png(filename='plot2.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+# Closing a graphic device
+dev.off()
